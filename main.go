7359316ac55c679f84e8c5cf9fcef5cf98a9be22
MarcosIgnacioo <magma_ruby145@hotmail.com>
2024-02-09 23:29:48 -0700

    prod

--- main.go
+++ main.go
@@ -3,15 +3,23 @@ package main
 import (
 	"github.com/MarcosIgnacioo/classmoodls/controllers"
 	"github.com/gin-gonic/gin"
+	"os"
 )
 
 func main() {
 	r := gin.Default()
 	r.LoadHTMLGlob("public/templates/*")
-	r.Static("/assets", "./assets")
+	r.Static("/assets", "/assets")
 	r.GET("/", controllers.LogIn)
 	r.GET("/wa", controllers.Test)
 	r.POST("/LogIn", controllers.LogInPost)
-	r.Run()
+	port := os.Getenv("STATE")
+	if port == "dev" {
+		port = "3000"
+	} else {
+		gin.SetMode(gin.ReleaseMode)
+		port = "3030"
+	}
+	r.Run("0.0.0.0:" + port)
 	// pw.FuckAround("marcosignc_21", "sopitasprecio")
 }
