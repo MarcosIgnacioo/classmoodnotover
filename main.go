4aab7232e419d27833feaedd55dd21f8765a4ca5
MarcosIgnacioo <magma_ruby145@hotmail.com>
2024-02-09 23:51:01 -0700

    whatsap

--- main.go
+++ main.go
@@ -3,24 +3,28 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/MarcosIgnacioo/classmoodls/controllers"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		fmt.Println("error con path")
+		return
+	}
 	r := gin.Default()
-	r.LoadHTMLGlob("public/templates/*")
-	r.Static("/assets", "/assets")
+	r.LoadHTMLGlob(dir + "public/templates/*")
+	r.Static("/assets", dir+"/assets")
 	r.GET("/", controllers.LogIn)
 	r.GET("/wa", controllers.Test)
 	r.POST("/LogIn", controllers.LogInPost)
 	port := os.Getenv("STATE")
 	if port == "dev" {
-		fmt.Println("dev")
 		port = "3000"
 	} else {
-		fmt.Println("prod")
 		port = "3030"
 	}
 	r.Run("0.0.0.0:" + port)
